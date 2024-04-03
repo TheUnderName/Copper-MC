cea78b943880d29507848dc0082509146683571c
TheUnderName <miadgamerprooo@gmail.com>
2024-04-03 15:29:02 +0330

    add

--- src/handle/OpenConnectionRequest2.hx
+++ src/handle/OpenConnectionRequest2.hx
@@ -1,5 +1,6 @@
 package handle;
 
+import haxe.Exception;
 import haxe.io.Bytes;
 import haxe.io.BytesInput;
 import haxe.io.BytesOutput;
@@ -12,17 +13,38 @@ class OpenConnecetRequest2 {
     private var Buffer:Bytes;
     private var MTU:Bytes;
     private var ClientGUID:Int;
-    private var BytesInput:BytesInput;
-
+    private var BytesInputs:BytesInput;
+    private var AdrVersion:Int;
+    private var AdrByte:Bytes;
+    private var IpADDR:String;
+    private var AddreasPort:Int;
     public function new(Buffer:Bytes) {
         this.Buffer = Buffer;
     }
 
     public function decode() {
-        BytesInput = new BytesInput(Buffer);
-        PACKET_ID = BytesInput.readByte();
+        BytesInputs = new BytesInput(Buffer);
+        PACKET_ID = BytesInputs.readByte();
         Logger.Debug("---Open Connection Request 2 ----");
         Logger.Debug(PACKET_ID);
+        MAGIC = BytesInputs.read(16);
+        Logger.Debug(MAGIC.toHex());
+        AdrVersion = BytesInputs.readByte();
+        if (AdrVersion == 4) {
+            Logger.Debug(AdrVersion);
+            var ipBytes:Array<Int> = [];
+            for (i in 0...4) {
+                ipBytes.push(BytesInputs.readByte()); // problam
+            }
+            var IpADDR:String = ipBytes.join(".");
+            var AddreasPort:Int = BytesInputs.readUInt16();
+            Logger.Debug(IpADDR + ":" + AddreasPort);
+        } else {
+            Logger.Debug("ipv6 not supported");
+            return;
+        }
+
+        
     
     }
 
